e547713e6347b56df6b931ec0273fb9a3bb8cbf5
trumk <nguyentrungtest2292003@gmail.com>
2024-09-29 21:33:18 +0700

    setup backend

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Helloooooooooo</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.header}>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    backgroundColor: "green",
+    fontSize: 40,
+    paddingHorizontal: 20,
+    textAlign: "center"
   },
+  container: {
+    paddingTop: 40,
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  }
 });
